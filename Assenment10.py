1ff1a4d5316042d3f4009a6257f29203e27aab39
varsha1329 <110395330+varsha1329@users.noreply.github.com>
2022-08-02 21:05:39 +0530

    Add files via upload

---
+++ Assenment10.py
@@ -0,0 +1,6 @@
+
+Mixed_List = [-10, "Varsha", 918763867, "Nagput", 10, "BlackColor"]
+
+print("List ID Number and Values are:")
+for index, value in enumerate(Mixed_List):
+    print("ID Number = ", index, " and Value = ", value)
