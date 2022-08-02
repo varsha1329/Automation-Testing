2f7b140fc82ca82679618821416b635c86673cea
varsha1329 <110395330+varsha1329@users.noreply.github.com>
2022-08-02 21:13:13 +0530

    Add files via upload

---
+++ Assenment11.py
@@ -0,0 +1,9 @@
+
+#Write a Python program to append a list to the second list. 
+list_of_Color = ["Red", "Blue", "Yello", "Orange"]
+list_Student = ["Varsha", "Anisha", "Gaurav"]
+
+for stud in list_Student:
+    list_of_Color.append(stud)
+
+print(list_of_Color)
