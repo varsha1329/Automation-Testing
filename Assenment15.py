635099c1560b77dea2d562aaafa4bc9f9fa69265
varsha1329 <110395330+varsha1329@users.noreply.github.com>
2022-08-02 21:53:20 +0530

    Add files via upload

---
+++ Assenment15.py
@@ -0,0 +1,6 @@
+Given_List = [11, -5, 83, 40, 0, 101, 65, 8, 6, 2]
+Given_List.sort()
+print('Ascending Order: ', Given_List)
+
+Given_List.sort(reverse=True)
+print('Descending Order: ', Given_List)
